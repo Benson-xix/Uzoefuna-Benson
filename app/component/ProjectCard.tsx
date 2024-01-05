22c66352789d9bd5586543d5550b54df1897dc5d
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2024-01-05 19:40:15 +0100

    Update ProjectCard.tsx

--- app/component/ProjectCard.tsx
+++ app/component/ProjectCard.tsx
@@ -15,10 +15,10 @@ const ProjectCard: React.FC<ProjectCardProps> = ({imgUrl, title, description, gi
     <div>
       <div  className="h-52 md:h-72 rounded-t-xl relative group" style={{background: `url(${imgUrl})`, backgroundSize: "cover"}}>
         <div className="overlay  items-center justify-center absolute top-0 lef-0 w-full h-full bg-[#181818]  bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
-            <Link href={"gitUrl"} className="h-14 w-14 mr-2   border-2 relative rounded-full border-orange-500 hover:border-white  group/link">
+            <Link href={gitUrl} className="h-14 w-14 mr-2   border-2 relative rounded-full border-orange-500 hover:border-white  group/link">
             <CodeBracketIcon className="h-10 w-10 text-orange-500  cursor-pointer group-hover/link:text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "/>
             </Link>
-            <Link href={"previewUrl"} className="h-14 w-14 border-2 relative rounded-full border-orange-500 hover:border-white  group/link">
+            <Link href={previewUrl} className="h-14 w-14 border-2 relative rounded-full border-orange-500 hover:border-white  group/link">
             <EyeIcon className="h-10 w-10 text-orange-500  cursor-pointer group-hover/link:text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "/>
             </Link>
         </div>
