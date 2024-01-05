e8235c263dfb373203accca49d467566056e26c5
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2024-01-05 21:45:56 +0100

    Update SkillComponent.tsx

--- app/component/SkillComponent.tsx
+++ app/component/SkillComponent.tsx
@@ -18,7 +18,8 @@ const SkillComponent = () => {
                 Using the Latest technologies the World has to Offer 
             </p>
             </div>
-        <Swiper
+         <div className='sm:block hidden w-[100%]'>
+                 <Swiper
         slidesPerView={5}
         loop={true}
         autoplay={{
@@ -27,7 +28,7 @@ const SkillComponent = () => {
         }}
         speed={5000}
         modules={[Autoplay]}
-        className="md:max-w-[100%] max-w-[80%] md:block hidden "
+        className="md:max-w-[100%] max-w-[100%] flex gap-[3rem] "
         >
             {SkillData.map((skill, index) => (
                 <SwiperSlide key={index} >
@@ -41,7 +42,11 @@ const SkillComponent = () => {
                 </SwiperSlide>
             ))}
         </Swiper>
-         <Swiper
+ 
+            </div>
+      
+            <div className='sm:hidden block w-[100%]'>
+            <Swiper
         slidesPerView={3}
         loop={true}
         autoplay={{
@@ -50,7 +55,7 @@ const SkillComponent = () => {
         }}
         speed={5000}
         modules={[Autoplay]}
-        className="md:max-w-[100%] max-w-[80%] md:hidden block"
+        className="md:max-w-[100%] max-w-[100%]  gap-[3rem]  "
         >
             {SkillData.map((skill, index) => (
                 <SwiperSlide key={index} >
@@ -64,7 +69,11 @@ const SkillComponent = () => {
                 </SwiperSlide>
             ))}
         </Swiper>
-        <Swiper
+            </div>
+            
+
+            <div className='sm:block hidden w-[100%]'>
+                <Swiper
         slidesPerView={5}
         loop={true}
         autoplay={{
@@ -74,7 +83,7 @@ const SkillComponent = () => {
         }}
         speed={5000}
         modules={[Autoplay]}
-        className="md:max-w-[100%] max-w-[80%] md:block hidden"
+        className="md:max-w-[100%] max-w-[100%] flex gap-[3rem] "
         >
             {SkillData.map((skill, index) => (
                 <SwiperSlide key={index}>
@@ -87,8 +96,11 @@ const SkillComponent = () => {
                     <p className='text-transparent bg-clip-text bg-gradient-to-r from-white via-orange-500 to-black hidden md:flex'>{skill.name}</p>
                 </SwiperSlide>
             ))}
-        </Swiper>
-          <Swiper
+        </Swiper> 
+            </div>
+            
+            <div className='sm:hidden block w-[100%]'>
+                <Swiper
         slidesPerView={3}
         loop={true}
         autoplay={{
@@ -98,7 +110,7 @@ const SkillComponent = () => {
         }}
         speed={5000}
         modules={[Autoplay]}
-        className="md:max-w-[100%] max-w-[80%] md:hidden block"
+        className="md:max-w-[100%] max-w-[100%] flex gap-[3rem] "
         >
             {SkillData.map((skill, index) => (
                 <SwiperSlide key={index}>
@@ -111,7 +123,8 @@ const SkillComponent = () => {
                     <p className='text-transparent bg-clip-text bg-gradient-to-r from-white via-orange-500 to-black hidden md:flex'>{skill.name}</p>
                 </SwiperSlide>
             ))}
-        </Swiper>
+        </Swiper> 
+            </div>
         </div>
     
     </div>
