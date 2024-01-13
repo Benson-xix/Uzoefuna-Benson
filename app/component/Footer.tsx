4c89a7ba2787cf97053efe0e327225667b5c97b2
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2024-01-12 22:21:45 -0900

    Update Footer.tsx

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -1,11 +1,13 @@
-
+import Link from "next/link"
 
 const Footer = () => {
   return (
     <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white ">
      <div className="container p-12 flex justify-between">
-    <span className="text-transparent bg-clip-text bg-gradient-to-r from-white via-slate-700 to-orange-500 font-semibold ">Software Developer</span>
-    <p className="text-orange-600">All rights reserved</p>
+     <div className="bg-gradient-to-l from-orange-400 via-orange-500 to-orange-600 w-[100px] h-[100px] rounded-full border-2 border-white flex justify-center items-center py-4 ">
+  <Link href={'/'} className="lg:text-5xl text-xl  text-transparent bg-clip-text bg-gradient-to-r from-white via-slate-700 to-white font-semibold  ">UB</Link>
+      </div>
+    <p className="text-orange-600 flex items-center">© 2024 Relax. All rights reserved</p>
       </div> 
     </footer>
   )
