d5a91520ea72408b369b3da93d53d5214911b744
uzoefuna benson dominic <uzoefunam.benson@gmail.com>
2024-01-12 22:23:37 -0900

    Update Footer.tsx

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 const Footer = () => {
   return (
-    <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white ">
-     <div className="container p-12 flex justify-between">
+    <footer className="footer w-full  border  border-t-[#33353F] border-l-transparent border-r-transparent text-white ">
+     <div className=" w-full p-12 flex justify-between">
      <div className="bg-gradient-to-l from-orange-400 via-orange-500 to-orange-600 w-[100px] h-[100px] rounded-full border-2 border-white flex justify-center items-center py-4 ">
   <Link href={'/'} className="lg:text-5xl text-xl  text-transparent bg-clip-text bg-gradient-to-r from-white via-slate-700 to-white font-semibold  ">UB</Link>
       </div>
